c7834374325ed1a7d88016080858e6ddb60282c6
Joseph Hall <joseph.hall@bristol.ac.uk>
2026-01-08 10:55:11 +0000

    Setting up the integrand

--- main.py
+++ main.py
@@ -12,21 +12,37 @@ H0 = 70
 COSMO = FlatLambdaCDM(H0, 0.3)
 LITLLE_H = H0/100
 
-print(COSMO.differential_comoving_volume(0.1))
 
 def xlf_schechter(lx, phi_star, lx_star, alpha):
     return (phi_star * ((lx/lx_star)**-alpha) * np.exp(-lx/lx_star) * 1/lx_star)
 
 
+def integrand(lx, z, phi_star, lx_star, alpha):
+    # Get comoving volume
+    comovol = COSMO.differential_comoving_volume(z)
+
+    # Get Schechter func value
+    phi_l = xlf_schechter(lx, phi_star, lx_star, alpha)
+
+    # Return integrand
+    return phi_l * comovol
+
+
 def main():
-    # Using WARPS XLF
+    # Using WARPS/REFLEX XLF 
     phi_star = 2.94e-7 * (u.Mpc ** -3)
     l_star = 2.64e44 * u.erg / u.second
     alpha = 1.69
     lumins = np.logspace(42, 45.3)*u.erg/u.second
 
+    # Define our Schechter function
+    phi = xlf_schechter(lumins, phi_star, l_star, alpha)
+
+    # Integrate Schechter with respect to L
+    print(integrand(lumins[0], 0.14, phi_star, l_star, alpha))
+
     fig, ax = plt.subplots()
-    ax.plot(lumins, xlf_schechter(lumins, phi_star, l_star, alpha))
+    ax.plot(lumins, phi)
     ax.set_yscale('log')
     ax.set_xscale('log')
     ax.set_xlabel("Luminosity [erg s^-1]")
